88f41ac5499e5b9627f48db3c1e1d713cd25a5f2
wpmohammad1 <alimohammadjhd@gmail.com>
2023-05-28 00:26:07 +0600

    send view details page data and tab view data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -26,12 +26,49 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db('toyDB').collection('toy')
+    app.post('/addtoy', async(req, res) =>{
+      const newToy = req.body;
+      console.log(newToy)
+      const result = await toyCollection.insertOne(newToy);
+      res.send(result)
+    })
+    app.get('/addtoy', async(req, res)=>{
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.get('/addtoy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const user = await toyCollection.findOne(query);
+      res.send(user)
+  })
+  app.get('/addtoy', async (req, res) => {
+    const sort =req.query.sort;
+    let query = {}
+    if (req.query?.email) {
+        query = { sellerEmail: req.query.email }
+    }
+    const options ={
+        sort:{
+            "price": sort ==='asc'? 1: -1
+        }
+    }
+    const result = await toyCollection.find(query,options).toArray();
+    res.send(result)
+})
+
+   
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
